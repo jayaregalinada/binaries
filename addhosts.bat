5f21e94182c62564d3361200a148882420b44807
jayaregalinada <jayaregalinada@gmail.com>
2017-07-10 16:59:07 -0700

    Have a default IP and added new line each.

--- addhosts.bat
+++ addhosts.bat
@@ -1,18 +1,20 @@
 @echo off
 title Add Hosts
 
-set arg1=%1
-set arg2=%2
+set url=%1
+set ip=%2
 
 IF "%1"=="" goto empty
-IF "%2"=="" goto empty
+IF "%2"=="" (
+    SET "ip=127.0.0.1"
+)
 
-echo %arg1% %arg2%
-echo %arg1%      %arg2% >> %WINDIR%\System32\drivers\etc\hosts
+echo %url% %ip%
+echo.%url%      %ip% >> %WINDIR%\System32\drivers\etc\hosts
 goto commonexit
 
 :empty
-echo "Usage: addhosts {IP} {URL}"
+echo "Usage: addhosts {URL} {IP [127.0.0.1]}"
 exit /b 1
 
 
